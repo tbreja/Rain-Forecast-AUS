db2163967b4ecec3c62160b2a9c46e4b12c9a8b0
Tubagus Moh Reja <73531678+tbreja@users.noreply.github.com>
2021-03-25 11:19:05 +0700

    Add files via upload

--- rain_forecast.py
+++ rain_forecast.py
@@ -15,7 +15,7 @@ def load(dataset):
     data = pd.read_csv(dataset)
     return data
 
-image = Image.open('rain_image.jpeg')
+image = Image.open('rain_image.jpg')
 Context = """The objective about this project is to predict wheter tomorrow willbe rain or not default based on several parameters. Because I am not an expert in meteorology and climatology domain, to prevent bias in choosing the threshold, I will make the result of this app from binary classification to percentage of posibilty default. With this simple app, the banker or guarantor will be more efficient to predict which application should be accepted or declined.
 The dataset is from the U.S. Small Business Administration (SBA) The U.S. SBA was founded in 1953 on the principle of promoting and assisting small enterprises in the U.S. credit market (SBA Overview and History, US Small Business Administration (2015)). Small businesses have been a primary source of job creation in the United States; therefore, fostering small business formation and growth has social benefits by creating job opportunities and reducing unemployment. 
 There have been many success stories of start-ups receiving SBA loan guarantees such as FedEx and Apple Computer. However, there have also been stories of small businesses or start-ups that have defaulted on their SBA guaranteed loans. \n Small business owners often seek out SBA (Small Business Association) loans because they guarantee part of the loan. Without going into too much detail, this basically means that the SBA will cover some of the losses should the business default on the loan, which lowers the risk involved for the business owner(s). This increases the risk to the SBA however, which can sometimes make it difficult to get accepted for one of their loan programs.
